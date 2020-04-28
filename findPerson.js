74f55ee773b26d4d14052e4ba4519a0de8e47047
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-28 20:04:10 -0300

    Finalizando testes unitários

--- findPerson.js
+++ findPerson.js
@@ -1,5 +1,29 @@
+const listaDePessoas = [
+    "José",
+    "Maria",
+    "Ana Maria"
+]
+
+
 const somaPessoas = (n1, n2) => n1 + n2;
 
+const encontrarPessoa = nome => {
+    try{
+        validacao(nome)
+        const pessoaEncontrada = listaDePessoas.find(pessoa => pessoa == nome)
+        return pessoaEncontrada ? pessoaEncontrada : "Pessoa não encontrada."
+    }
+    catch(erro){
+        return erro
+    }
+}
+
+const validacao = nome => {
+    if(!nome) throw "Campo nome vazio, item obrigatório."
+    if(typeof nome != "string") throw "Tipo inválido."
+}
+
 module.exports = {
-    somaPessoas
+    somaPessoas,
+    encontrarPessoa
 }
--- tests/findPersonListTest.js
+++ tests/findPersonListTest.js
@@ -8,5 +8,17 @@ describe("find person", () => {
     it("deve somar dois números e retornar 30", () => {
         findPerson.somaPessoas(10, 20).should.be.equal(30);
     })
+    it("Passar um nome que não esteja na lista, e retornar que não foi encontrado.", () => {
+        findPerson.encontrarPessoa("João da Silva").should.be.equal("Pessoa não encontrada.")
+    })
+    it("retorna a mensagem de que o nome está vazio, seu preenchimento é obrigatório", () => {
+        findPerson.encontrarPessoa().should.be.equal("Campo nome vazio, item obrigatório.")
+    })
+    it("Retornar mensagem de tipo inválido", () => {
+        findPerson.encontrarPessoa(123).should.be.equal("Tipo inválido.")
+    })
+    it("Deve retornar um nome da lista", () => {
+        findPerson.encontrarPessoa("Ana Maria").should.be.equal("Ana Maria")
+    })
 })
 
